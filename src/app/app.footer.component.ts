a71acf257895ad5bcbf680fa78aa18560062144e
Reedina <reedinafarms@gmail.com>
2018-02-10 09:01:47 -0800

    updated layout

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -3,11 +3,14 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-footer',
     template: `
+    <div class="mike-footer">
+    &nbsp;
         <div class="footer">
             <div class="card clearfix">
                 <span class="footer-text-left"> Cheetah Digital</span>
                 <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
             </div>
+        </div>
         </div>
     `
 })
